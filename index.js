c9c9d12bc7aedf00e844a7e779912a5a891ce402
GaryMa0725 <majjgary@gmail.com>
2021-08-12 22:25:24 +0800

    .js change

---
+++ index.js
@@ -0,0 +1,23 @@
+
+const selfInfo = {
+    data(){
+        return{
+            first: '1231231',
+            phone: '0912345678'
+        }
+
+    }
+}
+Vue.createApp(selfInfo).mount('#selfInfo')
+
+
+
+const selfInfo = {
+    data(){
+        return{
+            名稱   : '1231231',
+            價錢   : '0912345678'
+        }
+
+    }
+}
