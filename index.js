024fc9cefa9d1c406c1ad2920f80a5bd6c093f3a
GaryMa0725 <majjgary@gmail.com>
2021-08-16 19:22:49 +0800

    2021.8.14

--- index.js
+++ index.js
@@ -12,3 +12,16 @@ Vue.createApp(selfInfo).mount('#selfInfo')
 
 
 
+const itemDetail ={
+    data(){
+        return{
+            img:'/img/cancun.jpg',
+            imgAlt:'Beach in Cancun',   
+              eyebrow:'私人別墅',
+  title:'全部包到好的坎昆休閒度假村',
+  pricing:'一晚 $299 美金',
+  url:'/vacations/cancun'
+        }
+    }
+}
+Vue.createApp(itemDetail).mount('#itemDetail')
